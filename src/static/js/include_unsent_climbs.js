00e37e7c5c40c2bc94002618ecc718ad732a5238
Carlos Franzreb <>
2023-06-07 09:33:51 +0200

    Remove log

--- src/static/js/include_unsent_climbs.js
+++ src/static/js/include_unsent_climbs.js
@@ -4,6 +4,5 @@ let unsent_climbs_btn = document.getElementById("include-unsent-climbs");
 // Add an event listener to the checkbox
 unsent_climbs_btn.addEventListener("change", function() {
     INCLUDE_UNSENT_CLIMBS = unsent_climbs_btn.checked;
-    console.log(INCLUDE_UNSENT_CLIMBS);
     plot_data();
 });
