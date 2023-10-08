2cade6020e0765ece5c2249c56a248ed56476fb9
Carlos Franzreb <>
2023-10-08 13:58:55 +0200

    Display climbs in a table

--- climbs/routes.py
+++ climbs/routes.py
@@ -1,29 +1,17 @@
-from flask import Blueprint, render_template, url_for, redirect, send_from_directory
-
-from climbs.forms import FileForm
-from climbs.utils import format_file
-
+from flask import Blueprint, render_template, request
+from climbs.models import Route
 
 routes = Blueprint("routes", __name__)
 FILE = "src/static/data/boulders.csv"
 
 
-@routes.route("/", methods=["GET", "POST"])
-def home():
-    form = FileForm()
-    if form.validate_on_submit():
-        f = form.file.data
-        f.save(FILE)
-        return redirect(url_for("analysis"))
-    return render_template("home.html", title="Home", form=form)
+@routes.route("/table")
+def table():
+    page = request.args.get("page", 0, type=int)
+    climbs = Route.query.paginate(page=page, per_page=10)
+    return render_template("tables.html", climbs=climbs)
 
 
 @routes.route("/analysis")
 def analysis():
-    format_file(FILE)
     return render_template("analysis.html", title="Analysis")
-
-
-@routes.route("/data/<file>")
-def send_file(file):
-    return send_from_directory("static", f"data/{file}")
