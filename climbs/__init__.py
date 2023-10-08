792d6b962257d548edfcb646d88f5c47c4a869b8
Carlos Franzreb <>
2023-10-08 12:44:52 +0200

    Create debug dataset

--- climbs/__init__.py
+++ climbs/__init__.py
@@ -5,10 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(debug=False):
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///climbing_reports.db"
+    db_name = "climbs" if not debug else "debug"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_name}.db"
     db.init_app(app)
 
     from climbs.routes import routes
