5a0d4c4a28882fca8092cf0665e80185489143b4
Carlos Franzreb <>
2023-10-17 18:45:47 +0200

    Add a page for individual sessions

--- climbs/sessions/routes.py
+++ climbs/sessions/routes.py
@@ -10,3 +10,9 @@ def table_sessions() -> str:
     page = request.args.get("page", 1, type=int)
     page_sessions = Session.query.paginate(page=page, per_page=10)
     return render_template("sessions.html", sessions=page_sessions)
+
+
+@sessions.route("/session/<int:session_id>")
+def page_session(session_id: int) -> str:
+    session = Session.query.get(session_id)
+    return render_template("session.html", session=session)
