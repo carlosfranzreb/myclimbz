0cbd710c26cab7e382a5a31ba80034d8ca0f9b63
Carlos Franzreb <>
2023-10-17 17:24:28 +0200

    Add a page for individual routes

--- climbs/routes.py
+++ climbs/routes.py
@@ -6,28 +6,34 @@ FILE = "src/static/data/boulders.csv"
 
 
 @routes.route("/routes")
-def table_routes():
+def table_routes() -> str:
     page = request.args.get("page", 1, type=int)
     page_routes = Route.query.paginate(page=page, per_page=10)
     return render_template("routes.html", routes=page_routes)
 
 
+@routes.route("/route/<int:route_id>")
+def page_route(route_id: int) -> str:
+    route = Route.query.get(route_id)
+    return render_template("route.html", route=route)
+
+
 @routes.route("/areas")
-def table_areas():
+def table_areas() -> str:
     page = request.args.get("page", 1, type=int)
     page_areas = Area.query.paginate(page=page, per_page=10)
     return render_template("areas.html", areas=page_areas)
 
 
 @routes.route("/sessions")
-def table_sessions():
+def table_sessions() -> str:
     page = request.args.get("page", 1, type=int)
     page_sessions = Session.query.paginate(page=page, per_page=10)
     return render_template("sessions.html", sessions=page_sessions)
 
 
 @routes.route("/analysis")
-def analysis():
+def analysis() -> str:
     routes_dict = [route.as_dict() for route in Route.query.all()]
     grades_dict = [grade.as_dict() for grade in Grade.query.all()]
     return render_template(
