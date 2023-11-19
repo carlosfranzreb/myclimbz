685cfe2f382859f83b5cc7ad90b59e1682f89298
Carlos Franzreb <>
2023-11-19 16:25:37 +0100

    Sort areas by name

--- climbs/blueprints/areas/routes.py
+++ climbs/blueprints/areas/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session as flask_session
 from climbs.models import Area, Sector
 
 
@@ -8,10 +8,21 @@ areas = Blueprint("areas", __name__)
 @areas.route("/areas")
 def table_areas() -> str:
     page = request.args.get("page", 1, type=int)
-    page_areas = Area.query.paginate(page=page, per_page=10)
-    return render_template("areas.html", areas=page_areas)
+    sort_column = request.args.get("sort_column", None, type=str)
+
+    if sort_column is not None:
+        attribute = getattr(Area, sort_column)
+        if flask_session.pop("last_sort_column", None) == sort_column:
+            attribute = attribute.desc()
+        else:
+            flask_session["last_sort_column"] = sort_column
+        page_areas = Area.query.order_by(attribute).paginate(page=1, per_page=20)
+    else:
+        page_areas = Area.query.paginate(page=page, per_page=20)
+    return render_template("areas.html", title="Areas", areas=page_areas)
 
 
+@areas.route
 @areas.route("/area/<int:area_id>")
 def page_area(area_id: int) -> str:
     area = Area.query.get(area_id)
