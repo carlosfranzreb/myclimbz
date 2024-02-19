a3e8d39d31dcbb97a7e51e6dc68ac7612805db00
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2024-02-19 20:59:29 +0100

    Fix merging issues

--- run.py
+++ run.py
@@ -2,13 +2,3 @@ from climbz import create_app
 
 
 app = create_app("test_100")
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--database", help="database to use", default="prod")
-
-    args = parser.parse_args()
-
-    app = create_app(args.database)
-    app.run(debug=True)
