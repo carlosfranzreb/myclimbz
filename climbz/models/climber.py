01f5a99a22a3fc0868dfb0db9c03de3f69491027
Carlos Franzreb <>
2024-02-21 17:20:50 +0100

    Add user role

--- climbz/models/climber.py
+++ climbz/models/climber.py
@@ -1,11 +1,24 @@
+"""
+Represents a climber in the database. It is used to store the login information and
+personal data of the user.
+
+The role attribute is used to determine the permissions of the user. The options are:
+    0: regular user: cannot delete routes or areas, nor edit routes or areas that they
+        did not create
+    1: admin: can edit and delete routes and areas
+"""
+
 from flask_login import UserMixin
 
 from climbz import db, login_manager
 
 
 class Climber(db.Model, UserMixin):
-    # login information
+
     id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.Integer, nullable=False, default=0)
+
+    # login information
     email = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
