33b71724b22dc6bd024e3f2fafc6fbc76a0d336f
Carlos Franzreb <>
2024-03-23 14:33:17 +0100

    Remove PORT from env
    gcloud will inject it

--- run.py
+++ run.py
@@ -44,5 +44,5 @@ def check_request_validity():
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", False)
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=debug)
