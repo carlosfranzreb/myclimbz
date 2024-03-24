d548c6599a2b0832cdf6cf4c2c3e8d7a8e896a0d
Carlos Franzreb <>
2024-03-24 15:36:05 +0100

    Run tests with docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,75 +1,44 @@
-from multiprocessing import Process
-from time import sleep
+import os
 
 import pytest
-from flask.testing import FlaskClient
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 
-from climbz import create_app, db
+@pytest.fixture(scope="session")
+def db_session() -> Session:
+    """Load the test database and create a session."""
+    engine = create_engine("sqlite:///instance/test_100.db")
+    session = Session(engine)
+    return session
 
 
-@pytest.fixture()
-def app():
-    app = create_app(db_name="test_100")
-    app.config.update({"TESTING": True})
-    yield app
+@pytest.fixture(scope="session", autouse=True)
+def docker():
+    """Starts the Docker container and terminates it after the test."""
+    try:
+        os.system("docker compose up -d")
+        login()
+        yield
+    finally:
+        os.system("docker compose down")
 
 
-@pytest.fixture
-def test_client_db():
-    """Returns a test client for the app"""
-    app = create_app(db_name="test_100")
-    app.testing = True
-    with app.test_client() as test_client:
-        with app.app_context():
-            db.create_all()
-            yield test_client
-
-
-@pytest.fixture()
-def test_client() -> FlaskClient:
-    """Returns a test client for the app, closing it after the test."""
-    app = create_app(db_name="test_100")
-    app.testing = True
-    return app.test_client()
-
-
-def run_app() -> None:
-    app = create_app(db_name="test_100")
-    app.run()
-
-
-@pytest.fixture()
-def driver() -> webdriver.Chrome:
-    """Starts the Chrome webdriver and terminates it after the test."""
-
-    # start the app
-    app_process = Process(target=run_app)
-    app_process.start()
-    sleep(10)
-
-    # start the driver
+def login():
+    # start a headless Chrome browser
     driver_options = webdriver.ChromeOptions()
     driver_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=driver_options)
+    driver.get("http://127.0.0.1:5000/login")
+    WebDriverWait(driver, 10).until(EC.title_is("Login"))
 
-    # login
-    login_url = "http://localhost:5000/login"
-    driver.get(login_url)
-    WebDriverWait(driver, 10).until(lambda driver: driver.current_url == login_url)
-    driver.find_element(By.XPATH, "//input[@id='email']").send_keys("c1@climbz.com")
-    driver.find_element(By.XPATH, "//input[@id='password']").send_keys("123")
-    driver.find_element(By.XPATH, "//input[@value='Submit']").click()
-    WebDriverWait(driver, 10).until(
-        lambda driver: driver.current_url == "http://localhost:5000/"
-    )
-
-    # yield the driver and then quit it
-    try:
-        yield driver
-    finally:
-        driver.quit()
-        app_process.terminate()
+    # log in
+    driver.find_element(By.ID, "email").send_keys("c1@climbz.com")
+    driver.find_element(By.ID, "password").send_keys("123")
+    driver.find_element(By.XPATH, "//input[@type='submit']").click()
+    WebDriverWait(driver, 10).until(EC.title_is("Routes"))
+    driver.quit()
