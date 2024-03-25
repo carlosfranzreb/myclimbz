b85fc9fe392dd55c9113dfca31f4619464d6e844
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2024-03-25 21:13:25 +0100

    Fix filters test and conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -45,7 +45,6 @@ def run_app() -> None:
 @pytest.fixture()
 def driver() -> webdriver.Chrome:
     """Starts the Chrome webdriver and terminates it after the test."""
-
     # start the app
     app_process = Process(target=run_app)
     app_process.start()
@@ -53,19 +52,24 @@ def driver() -> webdriver.Chrome:
 
     # start the driver
     driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument("--headless=new")
+    # driver_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=driver_options)
+    try:
 
-    # login
-    login_url = "http://localhost:5000/login"
-    driver.get(login_url)
-    WebDriverWait(driver, 10).until(lambda driver: driver.current_url == login_url)
-    driver.find_element(By.XPATH, "//input[@id='email']").send_keys("c1@climbz.com")
-    driver.find_element(By.XPATH, "//input[@id='password']").send_keys("123")
-    driver.find_element(By.XPATH, "//input[@value='Submit']").click()
-    WebDriverWait(driver, 10).until(
-        lambda driver: driver.current_url == "http://localhost:5000/"
-    )
+        # login
+        login_url = "http://localhost:5000/login"
+        driver.get(login_url)
+        WebDriverWait(driver, 10).until(lambda driver: driver.current_url == login_url)
+        driver.find_element(By.XPATH, "//input[@id='email']").send_keys("c1@climbz.com")
+        driver.find_element(By.XPATH, "//input[@id='password']").send_keys("123")
+        driver.find_element(By.XPATH, "//input[@value='Submit']").click()
+        WebDriverWait(driver, 10).until(
+            lambda driver: driver.current_url == "http://localhost:5000/"
+        )
+    except Exception as e:
+        driver.quit()
+        app_process.terminate()
+        raise e
 
     # yield the driver and then quit it
     try:
