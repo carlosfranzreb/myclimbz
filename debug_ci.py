79d646acd00cb73527e951ca1b26808f020277ea
Carlos Franzreb <>
2024-04-16 10:58:35 +0200

    test

--- debug_ci.py
+++ debug_ci.py
@@ -7,18 +7,19 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pyvirtualdisplay import Display
-import chromedriver_autoinstaller
+
+# from pyvirtualdisplay import Display
+# import chromedriver_autoinstaller
 
 
 HOME_URL = "http://127.0.0.1:5000"
 
 
-chromedriver_autoinstaller.install()
+# chromedriver_autoinstaller.install()
 
 
-display = Display(visible=0, size=(800, 800))
-display.start()
+# display = Display(visible=0, size=(800, 800))
+# display.start()
 
 
 response = requests.get(HOME_URL)
