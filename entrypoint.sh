a099b606f49aa03342f25f5aed3ee02a29322abc
Carlos Franzreb <>
2024-04-16 11:19:14 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ "$CREATE_DB" -eq 1 ]; then
-    echo "Creating database..."
-    if [ "$CLIMBZ_DB_URI" != "sqlite:///test_100.db" ]; then
-        echo "Error: CLIMBZ_DB_URI must be set to 'sqlite:///test_100.db' when CREATE_DB is set to 1"
-        exit 1
-    fi
-    python create_db.py --test
-fi
-
 if [ "$FLASK_DEBUG" -eq 0 ]; then
     echo "Running in production mode"
     DEBUGPY="-m debugpy --listen 0.0.0.0:5670"
