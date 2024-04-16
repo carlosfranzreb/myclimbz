806a608185c7c65ed28f9d05b029f6e192e622b1
Carlos Franzreb <>
2024-04-16 11:10:00 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "FLASK_DEBUG: $FLASK_DEBUG"
+if [ "$CREATE_DB" -eq 1 ]; then
+    if [ "$CLIMBZ_DB_URI" != "sqlite:///test_100.db" ]; then
+        echo "Error: CLIMBZ_DB_URI must be set to 'sqlite:///test_100.db' when CREATE_DB is set to 1"
+        exit 1
+    fi
+    python create_db.py --test
+fi
 
 if [ "$FLASK_DEBUG" -eq 0 ]; then
     DEBUGPY="-m debugpy --listen 0.0.0.0:5670"
