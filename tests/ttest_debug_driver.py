4d06576ac2bd7409b6d66d71d70b02b3557980b4
Carlos Franzreb <>
2024-04-27 18:24:21 +0200

    Fix

--- tests/ttest_debug_driver.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-def test_request(driver: webdriver.Chrome):
-    btn = driver.find_element(By.XPATH, "//input[@id='display-form-toggle']")
-    if not btn.is_selected():
-        btn.find_element(By.XPATH, "..").click()
-        assert btn.is_selected()
