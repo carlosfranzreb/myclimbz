e5f62b334e057ee305feb80c1d2ffd462b3984f4
Carlos Franzreb <>
2024-05-22 16:16:24 +0200

    Remove predictions table

--- climbz/models/__init__.py
+++ climbz/models/__init__.py
@@ -3,7 +3,6 @@ Gathers all the models in the climbs app. They are sorted by their dependencies,
 the models that depend on others are imported after the models they depend on.
 """
 
-from .predictions import Predictions  # noqa
 from .rock_type import RockType  # noqa
 from .grade import Grade  # noqa
 from .crux import Crux  # noqa
