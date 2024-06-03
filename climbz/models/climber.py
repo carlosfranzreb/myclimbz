5fda29f0d217e67210f7b8c750513fb12d37f1c4
Carlos Franzreb <30505353+carlosfranzreb@users.noreply.github.com>
2024-06-03 08:09:43 +0100

    Version 1.5.0
    * Add logo and icon
    
    * Improve icon
    
    * Add climber card and sessions
    
    * Don't display actions columns if there aren't any
    
    * Admins can't edit climbs and sessions of other climbers
    
    * Display climber in session page
    
    * Fix actions column
    
    * Add table with climbers
    
    * Formatting
    
    * Version 1.5.0
    
    ---------
    
    Co-authored-by: Carlos Franzreb <>

--- climbz/models/climber.py
+++ climbz/models/climber.py
@@ -56,6 +56,17 @@ class Climber(db.Model, UserMixin):
     )
 
     @property
+    def year_started_climbing_str(self) -> str:
+        """Return the year the climber started climbing as a string."""
+        if self.year_started_climbing is None:
+            if self.sessions:
+                return str(self.sessions[0].date.year)
+            else:
+                return "N/A"
+        else:
+            return str(self.year_started_climbing)
+
+    @property
     def highest_grade_str(self) -> str:
         """Return the highest grade sent by the climber."""
         if not self.climbs or len(self.climbs) == 0:
@@ -67,7 +78,7 @@ class Climber(db.Model, UserMixin):
                 if opinion is not None and opinion.grade is not None:
                     if opinion.grade.level > max_grade.level:
                         max_grade = opinion.grade
-            return f"{max_grade.font} / {max_grade.hueco}"
+            return max_grade.user_grade
 
     @property
     def n_sends(self) -> int:
