ade35af6e9002a9a1be53d951c1e2fbb6ef86893
Carlos Franzreb <>
2025-11-20 15:13:09 +0100

    Increase sleep time

--- tests/test_forms_videos.py
+++ tests/test_forms_videos.py
@@ -92,7 +92,7 @@ def test_add_video(driver, db_session, started_session_id) -> None:
         },
     )
     assert form_accepted
-    sleep(5)  # video is uploaded in the background
+    sleep(10)  # video is uploaded in the background
 
     driver.save_screenshot("video_submitted.png")
 
